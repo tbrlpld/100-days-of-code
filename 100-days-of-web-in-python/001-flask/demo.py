17456df94b023d74bbef18b36d42ec7efc5d61e0
Tibor Leupold <tibor@lpld.io>
2019-10-17 09:42:08 -1000

    Moved the work directory into the course repo
    Before, the logs and the work was in this repo, while the course repo
    was just an unmodified fork of the original course template.
    That made the course repo kind of useless to have. Also, I might
    work on more than just the #100DayOfWeb course during my #100DaysOfCode.
    
    Thus, it makes more sense for me to keep the course work in the course
    repo and other works in their respective repos. This repo will only
    contain my logs. Which is how it was intended in the first place.

--- 100-days-of-web-in-python/001-flask/demo.py
+++
@@ -1,5 +0,0 @@
-from program import app  # this is actually accessing what's in the __init__.py
-
-
-
-
--- 100-days-of-web-in-python/001-flask/program/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-# Creates the flask instance
-app = Flask(__name__)
-
-# This needs to be below the app instance!
-from program import routes
-
